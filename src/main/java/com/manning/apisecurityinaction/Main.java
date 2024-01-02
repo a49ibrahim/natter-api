f24e1aa784093c59768b338f0ee26d76002ce2ca
Ali Ibrahim <a49ibrahim@gmail.com>
2024-01-01 21:32:52 -0800

    Hardening REST endpoints

--- src/main/java/com/manning/apisecurityinaction/Main.java
+++ src/main/java/com/manning/apisecurityinaction/Main.java
@@ -9,8 +9,6 @@ import org.h2.jdbcx.JdbcConnectionPool;
 import org.json.*;
 import spark.*;
 
-
-
 import static spark.Spark.*;
 
 public class Main {
@@ -22,33 +20,59 @@ public class Main {
         createTables(database);
         datasource = JdbcConnectionPool.create(
             "jdbc:h2:mem:natter", "natter_api_user", "password");
+
         database = Database.forDataSource(datasource);
-       
-        // database = Database.forDataSource(datasource);
         var spaceController = new SpaceController(database);
+
+        before(((request, response) -> {
+            if (request.requestMethod().equals("POST") &&
+            !"application/json".equals(request.contentType())) {
+                halt(415, new JSONObject().put(
+                    "error", "Only application/json supported"
+                ).toString());
+            }
+        }));
+
+        afterAfter((request, response) -> {
+            response.type("application/json;charset=utf-8");
+            response.header("X-Content-Type-Options", "nosniff");
+            response.header("X-Frame-Options", "DENY");
+            response.header("X-XSS-Protection", "0");
+            response.header("Cache-Control", "no-store");
+            response.header("Content-Security-Policy",
+                    "default-src 'none'; frame-ancestors 'none'; sandbox");
+            response.header("Server", "");
+        });
+
         post("/spaces", spaceController::createSpace);
 
-        after((request, response) -> {
-            response.type("application/json");
-    
-        });
+        // Additional REST endpoints not covered in the book:
+        post("/spaces/:spaceId/messages", spaceController::postMessage);
+        get("/spaces/:spaceId/messages/:msgId",
+            spaceController::readMessage);
+        get("/spaces/:spaceId/messages", spaceController::findMessages);
+
+        var moderatorController =
+            new ModeratorController(database);
+        delete("/spaces/:spaceId/messages/:msgId",
+            moderatorController::deletePost);
 
         internalServerError(new JSONObject()
             .put("error", "internal server error").toString());
         notFound(new JSONObject()
             .put("error", "not found").toString());
 
-        Exception(IllegalArgumentException.class, Main::badRequest);
-        Exception(JSONException.class, Main::badRequest);
-        Exception(EmptyResultException.class, (e, request, response) -> response.status(404));
-
-      
+        exception(IllegalArgumentException.class, Main::badRequest);
+        exception(JSONException.class, Main::badRequest);
+        exception(EmptyResultException.class,
+            (e, request, response) -> response.status(404));
     }
 
-    private static void badRequest(Exception ex, Request request, Response response) {
-      response.status(400);
-      response.body(new JSONObject().put("error", ex.getMessage()).toString());
-    }
+  private static void badRequest(Exception ex,
+      Request request, Response response) {
+    response.status(400);
+    response.body(new JSONObject().put("error", ex.getMessage()).toString());
+  }
 
     private static void createTables(Database database) throws Exception {
         var path = Paths.get(
